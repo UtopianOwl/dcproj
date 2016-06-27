c1ddb8e138c0b26ad7da822eb1ffbdcda26a1aa0
Drew Nauman <din3@cornell.edu>
2016-06-27 16:05:00 -0600

    finished change password

--- client/app/change-password/change-password.controller.js
+++ client/app/change-password/change-password.controller.js
@@ -1,23 +1,33 @@
 var app;
 (function (app) {
     var ChangePasswordController = (function () {
-        function ChangePasswordController($rootScope, $state) {
+        function ChangePasswordController(UserService, $rootScope, $state) {
+            this.UserService = UserService;
             this.$rootScope = $rootScope;
             this.$state = $state;
         }
-        ChangePasswordController.prototype.changePassword = function(password) {
+        ChangePasswordController.prototype.changePassword = function (oldPassword, newPassword, repeatPassword) {
             var _this = this;
-            this.UserService.changePassword(this.user, password).then(function () {
-                _this.$state.go('main page');
-            }, function (err) {
-                console.error(err);
-                if (err.message)
-                    alert(err.message);
-            });
+//            if (oldPassword !== this.$rootScope.user.password) {
+//                alert("That is not the current password!");
+//                document.changePasswordForm.reset();
+//                document.changePasswordForm.oldPassword.focus();
+//            } else if (newPassword !== repeatPassword) {
+//                alert("New passwords do not match!");
+//                document.changePasswordForm.reset();
+//                document.changePasswordForm.oldPassword.focus();
+//            } else {
+                this.UserService.changePassword(this.$rootScope.user, newPassword).then(function () {
+                    _this.$state.go('main page');
+                }, function (err) {
+                    console.error(err);
+                    if (err.message)
+                        alert(err.message);
+                });
+//            }
         };
-        }
         return ChangePasswordController;
     }());
     app.ChangePasswordController = ChangePasswordController;
-    angular.module('app').controller('MainController', MainController);
+    angular.module('app').controller('ChangePasswordController', ChangePasswordController);
 })(app || (app = {}));
